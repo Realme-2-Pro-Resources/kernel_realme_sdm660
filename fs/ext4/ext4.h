5c4af0d1af3db00324db726e865bc9bccd3174a9
Li Xi <pkuelelixi@gmail.com>
2016-01-08 16:01:22 -0500

    ext4: add project quota support
    This patch adds mount options for enabling/disabling project quota
    accounting and enforcement. A new specific inode is also used for
    project quota accounting.
    
    [ Includes fix from Dan Carpenter to crrect error checking from dqget(). ]
    
    Signed-off-by: Li Xi <lixi@ddn.com>
    Signed-off-by: Dmitry Monakhov <dmonakhov@openvz.org>
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>
    Reviewed-by: Andreas Dilger <adilger@dilger.ca>
    Reviewed-by: Jan Kara <jack@suse.cz>
    Change-Id: I8dc8c019693b828def27c637583a83798017edd0
    Signed-off-by: dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>

--- fs/ext4/ext4.h
+++ fs/ext4/ext4.h
@@ -1291,7 +1291,7 @@ struct ext4_super_block {
 #endif
 
 /* Number of quota types we support */
-#define EXT4_MAXQUOTAS 2
+#define EXT4_MAXQUOTAS 3
 #if defined(CONFIG_PRODUCT_REALME_RMX1801) && defined(CONFIG_EXT4_ASYNC_DISCARD_SUPPORT)
 //yh@PSW.BSP.Storage.EXT4, 2018-11-26 add for ext4 async discard suppot
 struct discard_policy {

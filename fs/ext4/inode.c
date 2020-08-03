7bf1b05ff3d3ef2b7e8bb2884612fb9e29c27e76
ShubhamB <shubhamprince111@gmail.com>
2020-08-03 15:23:45 +0530

    fs: ext4: Add EXT4_ASYNC_DISCARD_SUPPORT
     * from realme Q kernel release
    
    Signed-off-by: ShubhamB <shubhamprince111@gmail.com>

--- fs/ext4/inode.c
+++ fs/ext4/inode.c
@@ -1251,7 +1251,10 @@ errout:
 		if (inode->i_nlink)
 			ext4_orphan_del(NULL, inode);
 	}
-
+#ifdef CONFIG_EXT4_ASYNC_DISCARD_SUPPORT
+//yh@PSW.BSP.Storage.EXT4, 2018-11-26 add for ext4 async discard suppot
+	ext4_update_time(EXT4_SB(inode->i_sb));
+#endif
 	return ret ? ret : copied;
 }
 
@@ -1368,7 +1371,10 @@ errout:
 		if (inode->i_nlink)
 			ext4_orphan_del(NULL, inode);
 	}
-
+#ifdef CONFIG_EXT4_ASYNC_DISCARD_SUPPORT
+//yh@PSW.BSP.Storage.EXT4, 2018-11-26 add for ext4 async discard suppot
+	ext4_update_time(EXT4_SB(inode->i_sb));
+#endif
 	return ret ? ret : copied;
 }
 
@@ -4020,6 +4026,10 @@ void ext4_truncate(struct inode *inode)
 
 	if (inode->i_size & (inode->i_sb->s_blocksize - 1))
 		ext4_block_truncate_page(handle, mapping, inode->i_size);
+#ifdef CONFIG_EXT4_ASYNC_DISCARD_SUPPORT
+	//yh@PSW.BSP.Storage.EXT4, 2018-11-26 add for ext4 async discard suppot
+	ext4_update_time(EXT4_SB(inode->i_sb));
+#endif
 
 	/*
 	 * We add the inode to the orphan list, so that if this

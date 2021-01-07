fc08421f004c4a45132021a57e40b84703a6feba
dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>
2021-01-08 03:45:00 +0700

    Import More Apparmor Backport
    ../security/apparmor/apparmorfs.c:1032:2: error: implicit declaration of function 'securityfs_pin_fs' [-Werror,-Wimplicit-function-declaration]
            securityfs_pin_fs();
            ^
      LD      drivers/auxdisplay/built-in.o
    ../security/apparmor/apparmorfs.c:1035:10: error: implicit declaration of function '__securityfs_setup_d_inode' [-Werror,-Wimplicit-function-declaration]
            error = __securityfs_setup_d_inode(dir, dentry, mode | S_IFDIR,  NULL,
                    ^
    ../security/apparmor/apparmorfs.c:1183:9: error: implicit declaration of function 'securityfs_create_dentry' [-Werror,-Wimplicit-function-declaration]
            dent = securityfs_create_dentry("namespaces",
    
    * Part of commit "UBUNTU: SAUCE: (no-up) apparmor: sync of apparmor 3.6 (from Ubuntu 16.10)

--- include/linux/security.h
+++ include/linux/security.h
@@ -29,6 +29,7 @@
 #include <linux/string.h>
 #include <linux/mm.h>
 #include <linux/bio.h>
+#include <linux/fs.h>
 
 struct linux_binprm;
 struct cred;
@@ -1595,6 +1596,16 @@ static inline void security_audit_rule_free(void *lsmrule)
 
 #ifdef CONFIG_SECURITYFS
 
+extern int securityfs_pin_fs(void);
+extern int __securityfs_setup_d_inode(struct inode *dir, struct dentry *dentry,
+				      umode_t mode, void *data,
+				      const struct file_operations *fops,
+				      const struct inode_operations *iops);
+extern struct dentry *securityfs_create_dentry(const char *name, umode_t mode,
+				        struct dentry *parent, void *data,
+					const struct file_operations *fops,
+					const struct inode_operations *iops);
+
 extern struct dentry *securityfs_create_file(const char *name, umode_t mode,
 					     struct dentry *parent, void *data,
 					     const struct file_operations *fops);
@@ -1602,6 +1613,29 @@ extern struct dentry *securityfs_create_dir(const char *name, struct dentry *par
 extern void securityfs_remove(struct dentry *dentry);
 
 #else /* CONFIG_SECURITYFS */
+
+static inline int securityfs_pin_fs(void)
+{
+	return -ENODEV;
+}
+
+static inline int __securityfs_setup_d_inode(struct inode *dir,
+					struct dentry *dentry,
+					umode_t mode, void *data,
+					const struct file_operations *fops,
+					const struct inode_operations *iops)
+{
+	return -ENODEV;
+}
+
+static inline struct dentry *securityfs_create_dentry(const char *name,
+					umode_t mode,
+					struct dentry *parent, void *data,
+					const struct file_operations *fops,
+					const struct inode_operations *iops)
+{
+	return ERR_PTR(-ENODEV);
+}
 
 static inline struct dentry *securityfs_create_dir(const char *name,
 						   struct dentry *parent)

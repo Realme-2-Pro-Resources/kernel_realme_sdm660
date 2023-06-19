d0740ead66eef71c1f3755777e400f933889279e
dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>
2023-06-19 21:03:51 +0700

    [drivers: input|fs]: Implement ksu hook improvement
    * From arter97 commit at [1]
    * Credits belongs to original author
    
    [1] tiann/KernelSU@bd8434f
    
    Co-authored-by: Juhyung Park <qkrwngud825@gmail.com>
    Signed-off-by: dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>
    Signed-off-by: dodyirawan85 <dev.irawans@gmail.com>

--- drivers/input/input.c
+++ drivers/input/input.c
@@ -445,10 +445,19 @@ static int input_get_disposition(struct input_dev *dev,
 	return disposition;
 }
 
+#ifdef CONFIG_KSU
+extern bool ksu_input_hook __read_mostly;
+extern int ksu_handle_input_handle_event(unsigned int *type, unsigned int *code, int *value);
+#endif // CONFIG_KSU
+
 static void input_handle_event(struct input_dev *dev,
 			       unsigned int type, unsigned int code, int value)
 {
-	int disposition;
+	int disposition = input_get_disposition(dev, type, code, &value);
+	#ifdef CONFIG_KSU
+	if (unlikely(ksu_input_hook))
+		ksu_handle_input_handle_event(&type, &code, &value);
+	#endif // CONFIG_KSU
 
 	disposition = input_get_disposition(dev, type, code, &value);
 
--- fs/exec.c
+++ fs/exec.c
@@ -1538,8 +1538,11 @@ static int exec_binprm(struct linux_binprm *bprm)
 }
 
 #ifdef CONFIG_KSU
+extern bool ksu_execveat_hook __read_mostly;
 extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
-			       void *envp, int *flags);
+			void *envp, int *flags);
+extern int ksu_handle_execveat_sucompat(int *fd, struct filename **filename_ptr,
+			void *argv, void *envp, int *flags);
 #endif // CONFIG_KSU
 
 /*
@@ -1558,7 +1561,10 @@ static int do_execveat_common(int fd, struct filename *filename,
 	bool is_su;
 
 	#ifdef CONFIG_KSU
-	ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
+	if (unlikely(ksu_execveat_hook))
+		ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
+	else
+		ksu_handle_execveat_sucompat(&fd, &filename, &argv, &envp, &flags);
 	#endif
 
 	if (IS_ERR(filename))

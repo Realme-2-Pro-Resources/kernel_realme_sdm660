457c3126de2ee248c1bea9cdb588916a640bd3d2
dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>
2023-06-19 21:03:51 +0700

    [drivers: input|fs]: Implement ksu hook improvement
    * From arter97 commit at [1]
    * Credits belongs to original author
    
    [1] tiann/KernelSU@bd8434f
    
    Co-authored-by: Juhyung Park <qkrwngud825@gmail.com>
    Signed-off-by: dodyirawan85 <dev.irawans@gmail.com>

--- fs/exec.c
+++ fs/exec.c
@@ -1534,8 +1534,11 @@ static int exec_binprm(struct linux_binprm *bprm)
 }
 
 #ifdef CONFIG_KSU
+extern bool ksu_execveat_hook __read_mostly;
 extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
-			       void *envp, int *flags);
+			void *envp, int *flags);
+extern int ksu_handle_execveat_sucompat(int *fd, struct filename **filename_ptr,
+			void *argv, void *envp, int *flags);
 #endif // CONFIG_KSU
 
 /*
@@ -1553,7 +1556,10 @@ static int do_execveat_common(int fd, struct filename *filename,
 	int retval;
 
 	#ifdef CONFIG_KSU
-	ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
+	if (unlikely(ksu_execveat_hook))
+		ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
+	else
+		ksu_handle_execveat_sucompat(&fd, &filename, &argv, &envp, &flags);
 	#endif
 
 	if (IS_ERR(filename))

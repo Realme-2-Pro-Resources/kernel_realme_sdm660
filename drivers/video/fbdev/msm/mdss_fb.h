7a65d46f6d1d4bae1dc5f7e1ec483ac7ed5236b6
dodyirawan85 <dev.irawans@gmail.com>
2024-06-10 22:05:04 +0000

    wip
    Signed-off-by: dodyirawan85 <dev.irawans@gmail.com>

--- drivers/video/fbdev/msm/mdss_fb.h
+++ drivers/video/fbdev/msm/mdss_fb.h
@@ -27,6 +27,12 @@
 #define MDSS_LPAE_CHECK(phys)	\
 	((sizeof(phys) > sizeof(unsigned long)) ? ((phys >> 32) & 0xFF) : (0))
 
+#ifdef VENDOR_EDIT
+//Gou shengjun@MultiMedia.Display.LCD.Stability, 2017/02/15,
+//add for 16051 read LCM window info
+#define PANEL_BLACK 0x01
+#define PANEL_WHITE 0x02
+#endif /*VEDNOR_EDIT*/
 #define MSM_FB_DEFAULT_PAGE_SIZE 2
 #define MFD_KEY  0x11161126
 #define MSM_FB_MAX_DEV_LIST 32
@@ -374,6 +380,10 @@ struct msm_fb_data_type {
 	bool pending_switch;
 	struct mutex switch_lock;
 	struct input_handler *input_handler;
+#ifdef VENDOR_EDIT
+/* Gou shengjun@PSW.MM.Display.LCD.Stability,2018/8/31,add for support fingerprint feature*/
+	bool oppo_commit_info;
+#endif /*VENDOR_EDIT*/
 };
 
 static inline void mdss_fb_update_notify_update(struct msm_fb_data_type *mfd)

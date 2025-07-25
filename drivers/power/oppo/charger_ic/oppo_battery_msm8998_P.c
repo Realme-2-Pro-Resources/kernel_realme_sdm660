6e203079a4de5080f6e6e093663cb76166bc6fe7
dodyirawan85 <dev.irawans@gmail.com>
2025-07-25 18:54:39 +0700

    drivers: power: oppo_battery_msm8998_P: Report Not Charging as Discharging for AOSP
    Co-authored-by: Art_Chen <chenxy0201@qq.com>
    Signed-off-by: dodyirawan85 <dev.irawans@gmail.com>

--- drivers/power/oppo/charger_ic/oppo_battery_msm8998_P.c
+++ drivers/power/oppo/charger_ic/oppo_battery_msm8998_P.c
@@ -6604,7 +6604,7 @@ static int smb2_batt_get_prop(struct power_supply *psy,
 			if (!g_oppo_chip->authenticate)
 				val->intval = POWER_SUPPLY_STATUS_NOT_CHARGING;
 			else
-				val->intval = g_oppo_chip->prop_status;
+				val->intval = g_oppo_chip->prop_status == POWER_SUPPLY_STATUS_NOT_CHARGING ? POWER_SUPPLY_STATUS_DISCHARGING : g_oppo_chip->prop_status;
 		} else {
 			rc = smblib_get_prop_batt_status(chg, val);
 		}
